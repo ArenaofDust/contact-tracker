45a44b7f6f0ac64617dd2c178aafa35bf2a157d2
ArenaofDust <149437984+ArenaofDust@users.noreply.github.com>
2024-08-15 17:51:07 -0400

    Added error page as errorelement

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
 import './index.css';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World!</div>
+    element: <Root />,
+    errorElement: <ErrorPage />
   }
 ])
 
